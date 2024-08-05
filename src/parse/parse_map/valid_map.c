5c7e568bf3395504c36de3fcc116adca587760cb
Carlos Astorga <databook.one@gmail.com>
2024-08-05 15:58:19 +0200

    end parsing - no norminette yet

--- src/parse/parse_map/valid_map.c
+++ src/parse/parse_map/valid_map.c
@@ -12,6 +12,57 @@
 
 #include "cub3d.h"
 
+int	its_playable(t_map	*map)
+{
+	int	count;
+
+	count = 0;
+	map->y = 0;
+	while (map->matrix[map->y])
+	{
+		map->x = 0;
+		while (map->matrix[map->y][map->x])
+		{
+			if (map->matrix[map->y][map->x] == '1')
+				count++;
+			map->x++;
+		}
+		if ((map->y + 1) < map->h)
+			map->y++;
+		else
+			break ;
+	}
+	if (count < 4)
+		return (1);
+	return (0);
+}
+
+int	only_characters_allowed(t_map	*map)
+{
+	map->y = 0;
+	while (map->matrix[map->y])
+	{
+		map->x = 0;
+		while (map->matrix[map->y][map->x])
+		{
+			if (map->matrix[map->y][map->x] != ' ' &&
+				map->matrix[map->y][map->x] != '0' &&
+				map->matrix[map->y][map->x] != '1' &&
+				map->matrix[map->y][map->x] != 'N' &&
+				map->matrix[map->y][map->x] != 'S' &&
+				map->matrix[map->y][map->x] != 'E' &&
+				map->matrix[map->y][map->x] != 'W')
+				return (1);
+			map->x++;
+		}
+		if ((map->y + 1) < map->h)
+			map->y++;
+		else
+			break ;
+	}
+	return (0);
+}
+
 int	is_one_player(t_map *map)
 {
 	int	count;
@@ -36,10 +87,7 @@ int	is_one_player(t_map *map)
 			break ;
 	}
 	if (count != 1)
-	{
 		return (1);
-	}
-	printf("count: %d\n", count);
 	return (0);
 }
 
@@ -71,10 +119,10 @@ void	valid_map(t_map *map)
 {
 	if (any_zero_or_space(map) == 1)
 		ft_errors(3);
-	else
-		ft_printf("Map is valid\n");
 	if (is_one_player(map) == 1)
-		ft_printf("Player is not valid!\n");
-	else
-		ft_printf("Player is valid\n");
+		ft_errors(3);
+	if (only_characters_allowed(map) == 1)
+		ft_errors(3);
+	if (its_playable(map) == 1)
+		ft_errors(3);
 }
