984890b7690ea8d892ca757b38471844e7270bce
Carlos Astorga <carlos.astorgax@gmail.com>
2024-08-11 14:38:11 +0200

    exist_path_elements & exist_elements done!

--- src/parse/parse_elements/exist_elem.c
+++ src/parse/parse_elements/exist_elem.c
@@ -51,33 +51,46 @@ int	exist_elements(t_lmap *lmap)
 	return (0);
 }
 
+int	looking_for_xpm(char **elements)
+{
+	int		i;
+	int		temp;
+
+	temp = 0;
+	i = 0;
+	while (elements[i] && temp < 4)
+	{
+		if (ft_strlen(elements[i]) > 3)
+		{
+			if (ft_strnstr(elements[i], ".xpm", ft_strlen(elements[i])) != NULL)
+				temp++;
+			if (temp == 4)
+			{
+				free_elements(elements);
+				return (0);
+			}
+		}
+		i++;
+	}
+	return (temp);
+}
+
 int	exist_path_elements(t_lmap *lmap)
 {
 	char	**elements;
-	int		i;
 	int		temp;
+	int		count;
 
 	temp = 0;
 	while (lmap)
 	{
 		elements = ft_split2(lmap->content);
-		i = 0;
-		while (elements[i] && temp < 4)
-		{
-			if (ft_strlen(elements[i]) > 3)
-			{
-				if (ft_strnstr(elements[i], ".xpm", ft_strlen(elements[i])) != NULL)
-					temp++;
-				if (temp == 4)
-				{
-					free_elements(elements);
-					return (0);
-				}
-			}
-			i++;
-		}
+		count = looking_for_xpm(elements);
+		temp = temp + count;
 		free_elements(elements);
 		lmap = lmap->next;
 	}
+	if (temp != 4)
+		ft_errors(3);
 	return (1);
 }
